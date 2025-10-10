a51ba53a97ccee16d43ce57d30248c464973c984
Joshua Nunn <joshuanunn@hotmail.co.uk>
2025-10-10 23:14:29 +0100

    Remove redundant tests

--- test/test_irgen.ml
+++ test/test_irgen.ml
@@ -48,26 +48,3 @@ let%expect_test "IR generation using chained unary operators" =
              dst = (Ir.Var "tmp.2")};
            (Ir.Return (Ir.Var "tmp.2"))]})
     |}]
-
-let%expect_test "IR generation test" =
-  let input = "int main(void) { return 1 > 3; }" in
-  let lexbuf = Lexing.from_string input in
-  let ast = Parser.prog Lexer.read lexbuf in
-  let ir, _ = Irgen.convert_prog ast in
-  print_endline (Ir.show_prog ir);
-  [%expect
-    {|
-    (Ir.Program
-       Ir.Function {name = "main";
-         body =
-         [Ir.Binary {op = Ir.LessThan; src1 = (Ir.Constant 8);
-            src2 = (Ir.Constant 9); dst = (Ir.Var "tmp.0")};
-           Ir.JumpIfZero {condition = (Ir.Var "tmp.0"); target = "false_label"};
-           Ir.Binary {op = Ir.GreaterThan; src1 = (Ir.Constant 1);
-             src2 = (Ir.Constant 3); dst = (Ir.Var "tmp.1")};
-           Ir.JumpIfZero {condition = (Ir.Var "tmp.1"); target = "false_label"};
-           Ir.Copy {src = (Ir.Constant 1); dst = (Ir.Var "tmp.2")};
-           Ir.Jump {target = "end"}; (Ir.Label "false_label");
-           Ir.Copy {src = (Ir.Constant 0); dst = (Ir.Var "tmp.2")};
-           (Ir.Label "end"); (Ir.Return (Ir.Var "tmp.2"))]})
-    |}]
