09a17e4f756429a3d219699facab9a905bdd15ba
Joshua Nunn <joshuanunn@hotmail.co.uk>
2025-10-26 11:31:03 +0000

    Rename first AST semantic pass for clarity

--- lib/io.ml
+++ lib/io.ml
@@ -43,7 +43,7 @@ let run_parser lexbuf =
 let run_validator lexbuf s_env =
   try
     let ast = Parser.prog Lexer.read lexbuf in
-    let ast = Ast_analysis.resolve_prog ast s_env in
+    let ast = Ast_resolution.resolve_prog ast s_env in
     print_endline (Ast.show_prog ast)
   with
   | Parser.Error ->
@@ -58,7 +58,7 @@ let run_validator lexbuf s_env =
 let run_irgen lexbuf s_env l_env =
   try
     let ast = Parser.prog Lexer.read lexbuf in
-    let ast = Ast_analysis.resolve_prog ast s_env in
+    let ast = Ast_resolution.resolve_prog ast s_env in
     let ir = Irgen.convert_prog ast l_env in
     print_endline (Ir.show_prog ir)
   with
@@ -74,7 +74,7 @@ let run_irgen lexbuf s_env l_env =
 let run_codegen lexbuf s_env l_env =
   try
     let ast = Parser.prog Lexer.read lexbuf in
-    let ast = Ast_analysis.resolve_prog ast s_env in
+    let ast = Ast_resolution.resolve_prog ast s_env in
     let ir = Irgen.convert_prog ast l_env in
     let asm = Codegen.compile_prog ir in
     let asm = Codegen_lower.lower_prog asm l_env in
@@ -93,7 +93,7 @@ let run_codegen lexbuf s_env l_env =
 let run_emit lexbuf s_env l_env =
   try
     let ast = Parser.prog Lexer.read lexbuf in
-    let ast = Ast_analysis.resolve_prog ast s_env in
+    let ast = Ast_resolution.resolve_prog ast s_env in
     let ir = Irgen.convert_prog ast l_env in
     let asm = Codegen.compile_prog ir in
     let asm = Codegen_lower.lower_prog asm l_env in
@@ -112,7 +112,7 @@ let run_emit lexbuf s_env l_env =
 let run_exe lexbuf output_path s_env l_env =
   try
     let ast = Parser.prog Lexer.read lexbuf in
-    let ast = Ast_analysis.resolve_prog ast s_env in
+    let ast = Ast_resolution.resolve_prog ast s_env in
     let ir = Irgen.convert_prog ast l_env in
     let asm = Codegen.compile_prog ir in
     let asm = Codegen_lower.lower_prog asm l_env in
